56aa3ce0358bd086f9c256508f53972d68d7acb3
Oli Wennell <oli.wennell@red-gate.com>
2023-02-24 16:16:56 +0000

    First commit

---
+++ oidc-test3/HomeController.cs
@@ -0,0 +1,85 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace oidc_test3;
+
+public class HomeController : ControllerBase
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return Ok("hello from index");
+    }
+    
+    [Authorize] // If not already authenticated this kicks off the process
+    public IActionResult Protected()
+    {
+        return Ok("hello from protected");
+    }
+
+    public IActionResult Privacy()
+    {
+        return Ok("hello from privacy");
+    }
+
+    public async Task Logout()
+    {
+        await HttpContext.SignOutAsync(OpenIdConnectDefaults.AuthenticationScheme);
+        await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return Ok("oopsie!");
+    }
+}
+
+// public class HomeController : Controller
+// {
+//     private readonly ILogger<HomeController> _logger;
+//
+//     public HomeController(ILogger<HomeController> logger)
+//     {
+//         _logger = logger;
+//     }
+//
+//     public IActionResult Index()
+//     {
+//         return View();
+//     }
+//     
+//     [Authorize] // If not already authenticated this kicks off the process
+//     public IActionResult Protected()
+//     {
+//         return View();
+//     }
+//
+//     public IActionResult Privacy()
+//     {
+//         return View();
+//     }
+//
+//     public async Task Logout()
+//     {
+//         await HttpContext.SignOutAsync(OpenIdConnectDefaults.AuthenticationScheme);
+//         await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+//     }
+//
+//     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+//     public IActionResult Error()
+//     {
+//         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+//     }
+// }
---
+++ oidc-test3/Program.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.Configure<CookiePolicyOptions>(options =>
+{
+    options.MinimumSameSitePolicy = SameSiteMode.None;
+});
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+})
+    .AddCookie()
+    .AddOpenIdConnect(options =>
+    {
+        options.Authority = Environment.GetEnvironmentVariable("OIDC_AUTHORITY");
+        options.ClientId = Environment.GetEnvironmentVariable("OIDC_CLIENTID");
+        options.ClientSecret = Environment.GetEnvironmentVariable("OIDC_CLIENTSECRET");
+        options.ResponseType = "code";
+
+        options.CallbackPath = new PathString("/callback");
+        options.SignedOutCallbackPath = new PathString("/signout");
+    });
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
