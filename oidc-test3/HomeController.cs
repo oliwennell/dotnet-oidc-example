2392a03dc1cd019e09c2dcd19861e28c8ae2ebe0
Oli Wennell <oli.wennell@red-gate.com>
2023-02-24 17:07:35 +0000

    Clean up and add explanatory comments

--- oidc-test3/HomeController.cs
+++ oidc-test3/HomeController.cs
@@ -19,18 +19,19 @@ public class HomeController : ControllerBase
 
     public IActionResult Index()
     {
-        return Ok("hello from index");
+        return Ok("hello from index, this is completely unauthenticated");
     }
     
     [Authorize] // If not already authenticated this kicks off the process
     public IActionResult Protected()
     {
-        return Ok("hello from protected");
+        return Ok("hello from protected, only authenticated identies should see this");
     }
-
-    public IActionResult Privacy()
+    
+    [Authorize(Policy = "IsAdminPolicy")]
+    public IActionResult AdminArea()
     {
-        return Ok("hello from privacy");
+        return Ok("hello from secret admin area, only admins should be able to see this");
     }
 
     public async Task Logout()
@@ -45,41 +46,3 @@ public class HomeController : ControllerBase
         return Ok("oopsie!");
     }
 }
-
-// public class HomeController : Controller
-// {
-//     private readonly ILogger<HomeController> _logger;
-//
-//     public HomeController(ILogger<HomeController> logger)
-//     {
-//         _logger = logger;
-//     }
-//
-//     public IActionResult Index()
-//     {
-//         return View();
-//     }
-//     
-//     [Authorize] // If not already authenticated this kicks off the process
-//     public IActionResult Protected()
-//     {
-//         return View();
-//     }
-//
-//     public IActionResult Privacy()
-//     {
-//         return View();
-//     }
-//
-//     public async Task Logout()
-//     {
-//         await HttpContext.SignOutAsync(OpenIdConnectDefaults.AuthenticationScheme);
-//         await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
-//     }
-//
-//     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-//     public IActionResult Error()
-//     {
-//         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-//     }
-// }
--- oidc-test3/Program.cs
+++ oidc-test3/Program.cs
@@ -1,12 +1,16 @@
-﻿using Microsoft.AspNetCore.Authentication.Cookies;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using oidc_test3;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+
+builder.Services.AddScoped<IClaimsTransformation, ClaimsTransformation>();
 
 builder.Services.Configure<CookiePolicyOptions>(options =>
 {
@@ -20,14 +24,28 @@ builder.Services.AddAuthentication(options =>
     .AddCookie()
     .AddOpenIdConnect(options =>
     {
+        // These values are created by the identity provider (I used console.cloud.google.com)
+        // Then I set them as env vars so I didn't check them in to git accidentally.
         options.Authority = Environment.GetEnvironmentVariable("OIDC_AUTHORITY");
         options.ClientId = Environment.GetEnvironmentVariable("OIDC_CLIENTID");
         options.ClientSecret = Environment.GetEnvironmentVariable("OIDC_CLIENTSECRET");
+        
         options.ResponseType = "code";
 
+        // This gets automatically implemented for us by .NET. It must match what's configured in the identity provider.
         options.CallbackPath = new PathString("/callback");
+        
         options.SignedOutCallbackPath = new PathString("/signout");
     });
+
+builder.Services.AddAuthorization(options =>
+{
+    // Define a policy we can use to restrict access to authenticated requests from admin users.
+    // The ClaimsTransformation class decides if an identity is an admin or not.
+    // If an identity is an admin, it gives it the "IsAdminClaim" claim. 
+    options.AddPolicy("IsAdminPolicy",
+        policyBuilder => policyBuilder.RequireAuthenticatedUser().RequireClaim("IsAdminClaim", "true").Build());
+});
 
 var app = builder.Build();
 
